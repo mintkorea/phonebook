52e36bc01bd6ddbd320807e4ef9532c5f12fab82
mintkorea <mint@mint.co.kr>
2026-03-28 03:17:09 +0900

    search_app.py 업데이트

--- search_app.py
+++ search_app.py
@@ -1,3 +1,5 @@
+print("프로그램 시작")
+
 data = [
     {"name": "카페", "building": "병원", "floor": 1},
     {"name": "응급실", "building": "병원", "floor": 1},
@@ -6,6 +8,15 @@ data = [
 while True:
     query = input("검색어 입력: ")
 
+    if query == "exit":
+        break
+
+    found = False
+
     for item in data:
         if query in item["name"]:
-            print(item["name"], "-", item["building"], item["floor"], "층")
+            print(f"{item['name']} - {item['building']} {item['floor']}층")
+            found = True
+
+    if not found:
+        print("결과 없음")
